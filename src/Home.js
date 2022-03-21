f490facfbc025f1d5549e0e26ad65eea8c825214
Shafin580 <shafinwork580@gmail.com>
2022-03-21 20:23:16 +0600

    fetching data from JSON server

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react'
+import BlogList from './BlogList';
+import useFetch from './useFetch';
+
+function Home() {
+  const {data:blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
+  return (
+    <div className="home">
+      {error && <h2>{error}</h2>}
+      {isPending && <h2>Loading...</h2>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs"></BlogList>}
+    </div>
+  )
+}
+export default Home
